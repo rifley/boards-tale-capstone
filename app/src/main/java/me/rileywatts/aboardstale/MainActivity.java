01deed1862be7fb3ca1955ffb07133c4362666d1
Riley Watts <rileywatts.yes@gmail.com>
2017-06-19 10:36:55 -0700

    create layout for main activity, create world activity

--- app/src/main/java/me/rileywatts/aboardstale/MainActivity.java
+++ app/src/main/java/me/rileywatts/aboardstale/MainActivity.java
@@ -1,13 +1,39 @@
 package me.rileywatts.aboardstale;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView mHeaderTextView;
+    private EditText mNameEditText;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        mNameEditText = (EditText) findViewById(R.id.playerNameEditText);
+        mStartButton = (Button) findViewById(R.id.startGameButton);
+        mHeaderTextView = (TextView) findViewById(R.id.headerTextView);
+        Typeface ghibliFont = Typeface.createFromAsset(getAssets(), "fonts/ghibli.ttf");
+        mHeaderTextView.setTypeface(ghibliFont);
+        mStartButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mStartButton) {
+            Intent intent = new Intent(MainActivity.this, World.class);
+        }
     }
 }
---
+++ app/src/main/java/me/rileywatts/aboardstale/ui/WorldActivity.java
@@ -0,0 +1,15 @@
+package me.rileywatts.aboardstale.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.rileywatts.aboardstale.R;
+
+public class WorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_world);
+    }
+}
