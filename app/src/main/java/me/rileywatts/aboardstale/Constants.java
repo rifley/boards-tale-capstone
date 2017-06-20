1f554b91a132a24fe08507f1d976f20386655f95
Riley Watts <rileywatts.yes@gmail.com>
2017-06-20 09:26:20 -0700

    finish level object model, begin test

--- app/src/main/java/me/rileywatts/aboardstale/Constants.java
+++ app/src/main/java/me/rileywatts/aboardstale/Constants.java
@@ -1,8 +1,12 @@
 package me.rileywatts.aboardstale;
 
+import me.rileywatts.aboardstale.models.Level;
+
 /**
  * Created by Guest on 6/20/17.
  */
 
 public class Constants {
+
+    public static Level TEST_ONE = new Level("Level One", "You see a bear and a stick.", true, "Stick", "Not very sturdy", true, "Bear", "Burly and Hairy", <"You die", "You live">, )
 }
--- app/src/main/java/me/rileywatts/aboardstale/models/Level.java
+++ app/src/main/java/me/rileywatts/aboardstale/models/Level.java
@@ -13,9 +13,11 @@ import java.util.List;
 public class Level {
     String title;
     String description;
-    String item;
+    Boolean item;
+    String itemName;
     String itemDescription;
-    String enemy;
+    Boolean enemy;
+    String enemyName;
     String enemyDescription;
     List<String> enemyOutcomes;
     Integer healthChange;
@@ -24,8 +26,64 @@ public class Level {
 
     public Level() {}
 
-    public Level(String title, String description, String item, String itemDescription, ) {
+    public Level(String title, String description, Boolean item, String itemName, String itemDescription, Boolean enemy, String enemyName, String enemyDescription, List<String> enemyOutcomes,Integer healthChange ) {
+        this.title = title;
+        this.description = description;
+        this.item = item;
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.enemy = enemy;
+        this.enemyName = enemyName;
+        this.enemyDescription = enemyDescription;
+        this.enemyOutcomes = enemyOutcomes;
+        this.healthChange = healthChange;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean checkItem() {
+        return item;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public Boolean checkEnemy() {
+        return enemy;
+    }
+
+    public String getEnemyName() {
+        return enemyName;
+    }
+
+    public String getEnemyDescription() {
+        return enemyDescription;
+    }
+
+    public List<String> getEnemyOutcomes() {
+        return enemyOutcomes;
+    }
+    public Integer getHealthChange() {
+        return healthChange;
+    }
+
+    public void setItem() {
+        item = false;
+    }
+
+    public void setEnemy() {
+        enemy = false;
     }
 
 }
