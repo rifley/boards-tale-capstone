d0b35bdc116978545d7cd78816229c58fd97cbe4
Riley Watts <rileywatts.yes@gmail.com>
2017-06-21 11:33:12 -0700

    connect dialogue fragment to world activity, begin styling dialogue layout

--- app/src/main/java/me/rileywatts/aboardstale/ui/WorldActivity.java
+++ app/src/main/java/me/rileywatts/aboardstale/ui/WorldActivity.java
@@ -26,7 +26,9 @@ public class WorldActivity extends AppCompatActivity {
         player = Parcels.unwrap(getIntent().getParcelableExtra("player"));
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         PlayerNavigationFragment navFragment = PlayerNavigationFragment.newInstance(player, level);
+        DialogueFragment dialogueFragment = DialogueFragment.newInstance(player, level);
         ft.replace(R.id.navDisplayFrame, navFragment);
+        ft.replace(R.id.dialogueDisplayFrame, dialogueFragment);
         ft.commit();
 
         Log.v("inside World", player.getName());
